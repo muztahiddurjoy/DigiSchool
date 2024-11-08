fd59c508f30f7ed229f63e156a4826e39cdff3d1
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-11-08 10:23:17 +0600

    created some containers

--- frontend/components/Home/counter-section.tsx
+++ frontend/components/Home/counter-section.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import CounterAdapter from './counter-adapter'
 
 const CounterContainer = () => {
   return (
-    <div>CounterContainer</div>
+    <div className='md:px-20 grid grid-cols-4 lg:px-44 my-20'>
+        <CounterAdapter desc='Students Enrolled' title='120k+'/>
+        <CounterAdapter desc='Total Schools' title='20k+'/>
+        <CounterAdapter desc='Teachers' title='10k+'/>
+        <CounterAdapter desc='Raised Donation (BDT)' title='120k'/>
+    </div>
   )
 }
 
--- frontend/components/common/navbar.tsx
+++ frontend/components/common/navbar.tsx
@@ -1,8 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 right-0">Navbar</div>
+    <div className="fixed top-0 left-0 right-0 backdrop-blur-lg md:px-20 py-3 flex items-center justify-between">
+      <p className='text-xl font-bold text-primary'>DigiSchool</p>
+      <ul className='flex items-center justify-center text-sm font-semibold text-primary'>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+      </ul>
+      <div className="flex justify-end items-center gap-1">
+        <Button  variant="outline">Login</Button>
+        <Button>Register</Button>
+      </div>
+    </div>
   )
 }
 
