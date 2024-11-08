062921e09a46f94ef48df1a83816bdc8af99be22
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-11-08 17:13:43 +0600

    add donor profile table component and update sidebar layout

--- frontend/app/profile/donor/page.tsx
+++ frontend/app/profile/donor/page.tsx
@@ -1,9 +1,30 @@
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import React from 'react'
 
 const DonorProfile = () => {
   return (
-    <div>
-        
+    <div className='px-10'>
+        <Table className='w-full'>
+          <TableCaption>A list of your transactions.</TableCaption>
+          <TableHeader className='bg-primary '>
+            <TableRow className='[&>*]:text-black'>
+              <TableHead className=''>Transaction ID</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Purpose</TableHead>
+              <TableHead>Method</TableHead> 
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell className="font-medium">{new Date().toLocaleDateString()}</TableCell>
+              <TableCell>Donated to Random Student</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">2.00 BDT</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
     </div>
   )
 }
