e6e7524fc360cb96da7ebae632aee5e765939f4d
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-11-08 10:40:30 +0600

    created sponsor container

--- frontend/components/Home/sponsor-companies.tsx
+++ frontend/components/Home/sponsor-companies.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const SponsorCompanies = () => {
   return (
-    <div>SponsorCompanies</div>
+    <div className='py-10 px-20'>
+      <p className='text-sm text-primary font-semibold'>Sponsors</p>
+      <p className="text-3xl font-bold">Sponsor Companies</p>
+      <div className="flex items-center justify-center flex-wrap gap-5 px-10 mt-10">
+        {Array(13).fill("_").map((_,i)=>
+        <Image src="/brac.jpeg" height={50} width={100} alt='Brac' className='filter transition-all ease-in-out duration-300 grayscale hover:grayscale-0' key={i}/>)}
+       </div>
+    </div>
   )
 }
 
