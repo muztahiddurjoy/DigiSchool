466e10597fb7bdbd72cc3f9ca6f22b55f2e35aab
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-11-08 16:51:56 +0600

    add setup school form component

---
+++ frontend/app/setup-school/page.tsx
@@ -0,0 +1,12 @@
+import SetupSchoolForm from '@/components/SetupSchool/setup-school-form'
+import React from 'react'
+
+const SetupSchool = () => {
+  return (
+    <div>
+        <SetupSchoolForm/>
+    </div>
+  )
+}
+
+export default SetupSchool
